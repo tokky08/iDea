e439e5645ea324ff520acc3bf451cf5fa2771a5a
TOKITA KAZUHITO <tokky.kz08@gmail.com>
2020-07-07 00:44:35 +0900

    agin機能の実装

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,3 @@
-from flask import Flask
-from flask import render_template
 from flask import *
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
@@ -10,18 +8,20 @@ import random
 
 app = Flask(__name__)
 
-@app.route('/hello/')
-@app.route('/hello/<name>')
-def hello(name=None):
-    return render_template('index.html', name=name)
-
 
 @app.route("/", methods=["GET", "POST"])
-def odd_even():
+def top():
     if request.method == "GET":
-        return render_template('index.html')
+        return render_template('top.html')
     else:
-        return scrayping(request.form["word"])
+        word = scrayping(request.form["word"])
+        return render_template('again.html', word=word)
+        
+@app.route("/again", methods=["GET", "POST"])
+def again():
+    word = scrayping(request.form["word"])
+    return render_template('again.html', word=word)
+        
 
 def scrayping(word):
     driver = webdriver.Chrome("driver/chromedriver")
@@ -30,12 +30,10 @@ def scrayping(word):
 
     driver.get("https://www.google.co.jp")
 
-    value = word
-
     # 検索窓にSeleniumと入力する
     selector = '#tsf > div:nth-child(2) > div.A8SBwf > div.RNNXgb > div > div.a4bIc > input'
     element = driver.find_element_by_css_selector(selector)
-    element.send_keys(value)
+    element.send_keys(word)
 
     # enterキーを押す
     element.send_keys(Keys.ENTER)
@@ -44,7 +42,7 @@ def scrayping(word):
     element = driver.find_element_by_class_name("card-section")
     elements = element.text
     elements = elements.split()
-    elements = [text for text in elements if not text == value]
+    elements = [text for text in elements if not text == word]
     
     result = random.choice(elements)
             
