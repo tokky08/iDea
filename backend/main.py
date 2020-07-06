1f9d68b9609f253f419f4cd07b1bb591ae9ce404
TOKITA KAZUHITO <tokky.kz08@gmail.com>
2020-07-06 23:39:41 +0900

    関連ワードを一つに絞る

--- backend/main.py
+++ backend/main.py
@@ -1,19 +1,36 @@
 from flask import Flask
-app = Flask(__name__)
-
+from flask import render_template
+from flask import *
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import time
+import random
 
 
-def scrayping():
+
+app = Flask(__name__)
+
+@app.route('/hello/')
+@app.route('/hello/<name>')
+def hello(name=None):
+    return render_template('index.html', name=name)
+
+
+@app.route("/", methods=["GET", "POST"])
+def odd_even():
+    if request.method == "GET":
+        return render_template('index.html')
+    else:
+        return scrayping(request.form["word"])
+
+def scrayping(word):
     driver = webdriver.Chrome("driver/chromedriver")
 
     time.sleep(1)
 
     driver.get("https://www.google.co.jp")
 
-    value = input()
+    value = word
 
     # 検索窓にSeleniumと入力する
     selector = '#tsf > div:nth-child(2) > div.A8SBwf > div.RNNXgb > div > div.a4bIc > input'
@@ -26,27 +43,15 @@ def scrayping():
     element = driver.find_element_by_id("brs")
     element = driver.find_element_by_class_name("card-section")
     elements = element.text
-
-    # print(elements)
-
-    # for text in elements:
-    #     if not text == value:
-    #         print(text)
+    elements = elements.split()
+    elements = [text for text in elements if not text == value]
+    
+    result = random.choice(elements)
             
     driver.quit()
 
-    return elements
+    return result
 
-@app.route('/')
-def hello():
-    name = scrayping()
-    # name = "Hello World"
-    return name
-
-@app.route('/good')
-def good():
-    name = "Good"
-    return name
 
 ## おまじない
 if __name__ == "__main__":
