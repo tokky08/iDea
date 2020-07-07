bf3c9f4243fd79aa256f5bd53cba8d81248e2d10
TOKITA KAZUHITO <tokky.kz08@gmail.com>
2020-07-07 12:04:34 +0900

    履歴機能実装

--- backend/main.py
+++ backend/main.py
@@ -8,19 +8,29 @@ import random
 
 app = Flask(__name__)
 
+logs = []
 
 @app.route("/", methods=["GET", "POST"])
 def top():
     if request.method == "GET":
+        global logs
+        logs = []
         return render_template('top.html')
     else:
         word = scrayping(request.form["word"])
+        logs.append(request.form["word"])
+        logs.append(word)
         return render_template('again.html', word=word)
         
 @app.route("/again", methods=["GET", "POST"])
 def again():
     word = scrayping(request.form["word"])
-    return render_template('again.html', word=word)
+    logs.append(word)
+    return render_template('again.html', word=word, logs=logs)
+
+@app.route("/log", methods=["GET", "POST"])
+def log():
+    return render_template('log.html', logs=logs)
         
 
 def scrayping(word):
